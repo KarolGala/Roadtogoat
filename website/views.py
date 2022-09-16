b896fc01c6b64a1cf9a6dfd63d1507a09577d9fc
Karol Gała <Karolgala27@gmail.com>
2022-09-16 20:40:58 +0200

    button functionality

--- website/views.py
+++ website/views.py
@@ -8,3 +8,17 @@ views = Blueprint('views', __name__)
 @views.route("/")
 def index():
     return render_template("index.html") 
+
+@views.route("/Kalistenika")
+def kalistenika():
+    return render_template("Kategorie/Kalistenika.html") 
+
+
+@views.route("/Trójbój")
+def trojboj():
+    return render_template("Kategorie/Trojboj.html") 
+
+@views.route("/Hybryda")
+def Hybrda():
+    return render_template("Kategorie/Hybryda.html") 
+
